7492c2ce9c50e90222307f0113ab6cb2e2e91ef7
prashantsinghshivbhooshansingh-ship-it <prashantsinghshivbhooshansingh@gmail.com>
2026-04-02 23:37:50 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center" ,marginTop:"100px"}}>
+<h1> website working</h1>
+<p>Prashant Singh Private vault</p>
+  </div>
+);
+}
